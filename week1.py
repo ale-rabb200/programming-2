8da49783dbd9ff9e4030dc0459181c3ccb89b8b6
ale-rabb200 <arabbi200@caledonian.ac.uk>
2023-08-19 16:08:39 +0100

    week1

---
+++ week1.py
@@ -0,0 +1,16 @@
+#import time
+#time.sleep(10)
+#for i in range(0,3):
+#    print("hello")
+names=[]
+#for i in range(0,5):
+  #  names.append(name)
+ #   name= input("enter 5 names ")
+#print (names)
+#for j in range (0,5):
+ #   print (names[j])
+import numpy as np
+a= np.array([[1,2,3,4,5],[6,7,8,9,10]])
+b=np.max(a,axis=1)
+#a = np.array([[1,2,3,],[4,5,6]]) 
+print (b)
